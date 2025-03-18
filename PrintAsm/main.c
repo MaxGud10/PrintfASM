46e62ec173dc5bdaa7af46f8da0724401cb1f9f7
Max <karatemaksim0110@gmail.com>
2025-03-18 20:18:28 +0300

    добавил %c, %b, %o, %x

--- PrintAsm/main.c
+++ PrintAsm/main.c
@@ -1,22 +1,10 @@
-#include <stdio.h>
+extern int myPrintfC (char* format, ...);
  
- int sum (int x, int y)
- {
-     printf("sum: %d\n", x + y);
- 
-     return x + y;
- }
+int main (void)
+{
+    myPrintfC ("Hello!, %c s", 'x');
+    myPrintfC ("binary number: %b, %o, %x", 11, 11, 11);
+    return 0;
+}
 
 // gcc -g -no-pie -Wl,-z,noexecstack main.o 0-Linux-nasm-64.o -o program
-
-// # Компиляция ассемблерного кода
-// nasm -f elf64 -l printfAsm.lst printfAsm.s
-
-// # Компиляция C-кода
-// gcc -c main.c -o main.o
-
-// # Компоновка объектных файлов
-// gcc main.o printfAsm.o -o main
-
-// # Запуск программы
-// ./main
